3e0d343854b6cd6e8c1ccfe24015d278d3d475d9
Chris Pan <chris.pan@cargosmart.com>
2021-06-22 16:12:35 +0800

    refactor:remove useless field at address

--- src/main/java/com/tw/academy/basic/$6_primitive_obsession/practiceOne/Address.java
+++ src/main/java/com/tw/academy/basic/$6_primitive_obsession/practiceOne/Address.java
@@ -1,12 +1,10 @@
 package com.tw.academy.basic.$6_primitive_obsession.practiceOne;
 
 public class Address {
-    private final String address;
     private String city;
     private String province;
 
     public Address(String address) {
-        this.address = address;
         this.city = address.substring(address.indexOf("省") + 1, address.indexOf("市"));
         this.province = address.substring(0, address.indexOf("省"));
     }
@@ -17,17 +15,5 @@ public class Address {
 
     public String getProvince() {
         return province;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    String getCity(String address) {
-        return address.substring(address.indexOf("省") + 1, address.indexOf("市"));
-    }
-
-    String getProvince(String address) {
-        return address.substring(0, address.indexOf("省"));
     }
 }
