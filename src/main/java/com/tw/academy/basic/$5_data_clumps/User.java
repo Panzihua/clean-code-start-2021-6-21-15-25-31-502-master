f023f47ca5df90c18420c709e829fc2f3a01595e
Chris Pan <chris.pan@cargosmart.com>
2021-06-22 15:57:06 +0800

    refactor: add Address class

--- src/main/java/com/tw/academy/basic/$5_data_clumps/User.java
+++ src/main/java/com/tw/academy/basic/$5_data_clumps/User.java
@@ -3,7 +3,7 @@ package com.tw.academy.basic.$5_data_clumps;
 public class User {
     private final String buyerName;
     private final String buyerPhoneNumber;
-    private final String buyerAddress;
+    private final String buyerAddress;  
 
     public User(String buyerName, String buyerPhoneNumber, String buyerAddress) {
         this.buyerName = buyerName;
